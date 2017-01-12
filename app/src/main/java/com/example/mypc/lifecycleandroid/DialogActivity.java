043518fb66cbf7827c3584994fe24c2b79544016
Việt <cnv471994@gmail.com>
2017-01-12 15:51:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mypc/lifecycleandroid/DialogActivity.java
@@ -0,0 +1,16 @@
+package com.example.mypc.lifecycleandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MyPC on 1/12/2017.
+ */
+
+public class DialogActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/mypc/lifecycleandroid/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.mypc.lifecycleandroid;
+
+import android.Manifest;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private final String TAG=getClass().getSimpleName();
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
+        setContentView(R.layout.activity_main);
+        addEnvent();
+        buttonClick();
+    }
+
+    private void addEnvent() {
+        button = (Button) findViewById(R.id.button);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                buttonClickDialog();
+                break;
+        }
+    }
+
+    private void buttonClick(){
+        button.setOnClickListener(this);
+    }
+    private void buttonClickDialog() {
+        Intent intent = new Intent(MainActivity.this,DialogActivity.class);
+        startActivity(intent);
+    }
+}
